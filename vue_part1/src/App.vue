57624042f60cfd657e9e6ce8ccd1639f189b19af
dongmin0877 <sayg1o1d@github.com>
2020-05-01 10:20:12 +0900

    Head,Convention + slider(modify)

---
+++ vue_part1/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Todo />
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo';
+export default {
+  name: 'App',
+  components: {
+    Todo,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './styles/main.scss';
+</style>
